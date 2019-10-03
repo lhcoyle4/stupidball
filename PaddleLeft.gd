df98cee5153b7f32556f75b5999a639c7d3feca7
Louis Coyle <louie@pop-os.localdomain>
2019-10-03 17:40:02 -0400

    added rotation to paddles, increased canvas size, added bumpers

--- PaddleLeft.gd
+++ PaddleLeft.gd
@@ -6,16 +6,16 @@ func _ready():
 	set_process_input(true)
 	
 func _physics_process(delta):
-	if rotation >= -180:
-		rotation = -180
+	if rotation >= 215:
+		rotation = 215
 	
-	if rotation <= -215:
-		rotation = -215
+	if rotation <= 180:
+		rotation = 180
 		
-	if Input.is_action_pressed("ui_right"):
-		rotation += 35
-	else:
+	if Input.is_action_pressed("ui_left"):
 		rotation -= 35
+	else:
+		rotation += 35
 
 	set_rotation_degrees(rotation)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
